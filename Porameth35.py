d996b93d9961239a44a2219cc1cd5e418dd204f9
Porameth14 <mr.porandearth@gmail.com>
2025-08-13 13:54:42 +0700

    Add files via upload

---
+++ Porameth35.py
@@ -0,0 +1,7 @@
+name = input("Your name: ")
+birth_year = input("Your BC Born (e.g., 1990): ")
+age = input("Your age: ")
+
+print(f"Name: {name}")
+print(f"Birth Year (AD): {birth_year}")
+print(f"Age: {age} age")
