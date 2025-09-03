3e994b23a4b599d8cef0a494e7ef01cd27c64a31
namsom23 <nthmsom48@gmail.com>
2025-09-03 13:25:15 +0700

    อัปเดตโค้ด

--- Porameth35.py
+++
@@ -1,7 +0,0 @@
-name = input("Your name: ")
-birth_year = input("Your BC Born (e.g., 1990): ")
-age = input("Your age: ")
-
-print(f"Name: {name}")
-print(f"Birth Year (AD): {birth_year}")
-print(f"Age: {age} age")
